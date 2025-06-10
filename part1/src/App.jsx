cbaff5a28c69dd443a16e0bc86f9d4b32ee309ca
AGUEH Barnabé Prudent Mahugnon <111515950+Barnab1@users.noreply.github.com>
2025-06-10 18:22:48 +0000

    Refactoring the code while doing exercises 1.4 of part1

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -32,27 +32,30 @@ const Footer = (props) =>{
 
 const App = ()=>{
 
-   const course = 'Half Stack application development'
-  const part1 = {
-    name: 'Fundamentals of React',
-    exercises: 10
-  }
-  const part2 = {
-    name: 'Using props to pass data',
-    exercises: 7
-  }
-  const part3 = {
-    name: 'State of a component',
-    exercises: 14
-  }
+   const course = 'Half Stack application development';
+
+   const parts = [
+    {
+      name: 'Fundamentals of React',
+      exercises: 10
+    },
+    {
+      name: 'Using props to pass data',
+      exercises: 7
+    },
+    {
+      name: 'State of a component',
+      exercises: 14
+    }
+]
 
   return (
     <div>
 <Header course ={course} />
 
-<Content informations = {[['Fundamentals of React',' ',10],["Using props to pass data",' ',7],["State of a component",' ',14]]} />
+<Content informations = {[[parts[0].name,' ',parts[0].exercises],[parts[1].name,' ',parts[1].exercises],[parts[2].name,' ',parts[1].exercises]]} />
 
-<Footer exercices = {part1.exercises + part2.exercises + part3.exercises} />
+<Footer exercices = {parts[0].exercises + parts[1].exercises + parts[2].exercises} />
 
     </div>
   )
