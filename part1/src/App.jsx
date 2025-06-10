44d185f4bff5e33a902e1046545ab2f6dc3886b7
AGUEH Barnabé Prudent Mahugnon <111515950+Barnab1@users.noreply.github.com>
2025-06-10 10:44:33 +0000

    Refactoring the Content Component to render parts instead of displaying informations by its own

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,13 +1,31 @@
+  const course = 'Hallf Stack application develpment';
+  const part1 = 'Fundamentals of React';
+  const exercises1 = 10;
+  const part2 = 'Usings props to pass data';
+  const exercises2 = 7;
+  const part3 = 'State of a component';
+  const exercises3 = 14;
+
 const Header = (props)=>{
   return (
     <h1>{props.course}</h1>
   )
 }
-const Content = (props)=>{
+
+const Part = (props)=>{
   return (
     <p>
-      {props.part} {props.exercice}
+      {props.informations}
     </p>
+  )
+}
+const Content = (props)=>{
+  return (
+    <div>
+      <Part informations = {[part1," ",exercises1]}/>
+      <Part informations = {[part2," ",exercises2]}/>
+      <Part informations = {[part3," ",exercises3]}/>
+    </div>
   )
 }
 
@@ -18,21 +36,13 @@ const Footer = (props) =>{
 }
 
 const App = ()=>{
-  const course = 'Hallf Stack application develpment';
-  const part1 = 'Fundamentals of React';
-  const exercises1 = 10;
-  const part2 = 'Usings props to pass data';
-  const exercises2 = 7;
-  const part3 = 'State of a component';
-  const exercises3 = 14;
+
 
   return (
     <div>
 <Header course ={course} />
 
-<Content part={part1} exercice={exercises1} />
-<Content part={part2} exercice={exercises2} />
-<Content part={part3} exercice={exercises3} />
+<Content />
 
 <Footer exercices = {exercises1 + exercises2 + exercises3} />
 
