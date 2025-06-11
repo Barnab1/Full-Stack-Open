32fed498adbec1156e59174d19c0bd84c0a351ca
AGUEH Barnabé Prudent Mahugnon <111515950+Barnab1@users.noreply.github.com>
2025-06-11 10:45:08 +0000

    Did the exercise 1-5 of part1

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,41 +1,46 @@
 
 const Header = (props)=>{
-  
+ // console.log(props);
   return (
     <h1>{props.course}</h1>
   )
 }
 
 const Part = (props)=>{
-  return (
+  return(
     <p>
-      {props.informations}
+      {props.name} {props.exercises}
     </p>
   )
 }
-const Content = (props)=>{
-  console.log(props);
+
+const Content = (props) =>{
+const parts = props['parts']
+
   return (
     <div>
-      <Part informations = {props.informations[0]} />
-      <Part informations = {props.informations[1]} />
-      <Part informations = {props.informations[2]} />
+      <Part name={parts[0].name} exercises= {parts[0].exercises}/>
+      <Part name={parts[1].name} exercises= {parts[1].exercises}/>
+      <Part name={parts[2].name} exercises= {parts[2].exercises}/>
     </div>
   )
 }
 
+
 const Footer = (props) =>{
+  const totalExercises = props['parts'][0].exercises + props['parts'][1].exercises + props['parts'][2].exercises;
   return (
-    <p>Number of exercices {props.exercices} </p>
+    <p>Number of exercices {totalExercises} </p>
   )
 }
 
 const App = ()=>{
 
-   const course = 'Half Stack application development';
-
-   const parts = [
-    {
+   const course =
+   {
+      name: 'Half Stack application development',
+      parts: [
+        {
       name: 'Fundamentals of React',
       exercises: 10
     },
@@ -47,19 +52,18 @@ const App = ()=>{
       name: 'State of a component',
       exercises: 14
     }
-]
+      ]
+  
+    }
 
-  return (
-    <div>
-<Header course ={course} />
-
-<Content informations = {[[parts[0].name,' ',parts[0].exercises],[parts[1].name,' ',parts[1].exercises],[parts[2].name,' ',parts[1].exercises]]} />
-
-<Footer exercices = {parts[0].exercises + parts[1].exercises + parts[2].exercises} />
-
-    </div>
-  )
-}
+      return (
+        <div>
+          <Header course = {course.name} />
+          <Content parts = {course.parts} />
+          <Footer parts = {course.parts} />
+        </div>
+      )
+  }
 
 
 export default App
