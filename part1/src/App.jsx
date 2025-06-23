d6500be0bb5b0f5b9fe58abc0709f1f026549068
AGUEH Barnabé Prudent Mahugnon <111515950+Barnab1@users.noreply.github.com>
2025-06-23 17:25:17 +0000

    Did exercice 1.6 of part1

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,68 +1,27 @@
+import {useState} from 'react';
 
-const Header = (props)=>{
- // console.log(props);
-  return (
-    <h1>{props.course}</h1>
-  )
-}
+const Button = ({onClick, text})=> <button onClick={onClick}>{text}</button>
 
-const Part = (props)=>{
-  return(
-    <p>
-      {props.name} {props.exercises}
-    </p>
-  )
-}
-
-const Content = (props) =>{
-const parts = props['parts']
+const App = ()=>{
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
 
   return (
     <div>
-      <Part name={parts[0].name} exercises= {parts[0].exercises}/>
-      <Part name={parts[1].name} exercises= {parts[1].exercises}/>
-      <Part name={parts[2].name} exercises= {parts[2].exercises}/>
+      <h1>give feedback</h1>
+      <Button onClick ={()=>setGood(good+1)} text="good"/>
+      <Button onClick ={()=>setNeutral(neutral+1)} text="neutral"/>
+      <Button onClick ={()=>setBad(bad +1)} text="bad"/>
+
+      <h2>Statistics</h2>
+
+      <div>good {good}</div>
+      <div>neutral {neutral} </div>
+      <div>bad {bad}</div>
     </div>
   )
-}
-
-
-const Footer = (props) =>{
-  const totalExercises = props['parts'][0].exercises + props['parts'][1].exercises + props['parts'][2].exercises;
-  return (
-    <p>Number of exercices {totalExercises} </p>
-  )
-}
-
-const App = ()=>{
-
-   const course =
-   {
-      name: 'Half Stack application development',
-      parts: [
-        {
-      name: 'Fundamentals of React',
-      exercises: 10
-    },
-    {
-      name: 'Using props to pass data',
-      exercises: 7
-    },
-    {
-      name: 'State of a component',
-      exercises: 14
-    }
-      ]
-  
-    }
-
-      return (
-        <div>
-          <Header course = {course.name} />
-          <Content parts = {course.parts} />
-          <Footer parts = {course.parts} />
-        </div>
-      )
   }
 
 
