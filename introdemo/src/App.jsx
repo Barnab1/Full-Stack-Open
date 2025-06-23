618408d53543da4babf36d6064dad8369ff9fa3b
AGUEH Barnabé Prudent Mahugnon <111515950+Barnab1@users.noreply.github.com>
2025-06-23 10:45:51 +0000

    Worked until section -Funtion that return function - for part1 course

--- introdemo/src/App.jsx
+++ introdemo/src/App.jsx
@@ -1,49 +1,32 @@
 import {useState} from 'react';
 
+const hello = (who)=>{
 
-const History = ({allClicks}) =>{
-  if(allClicks.length === 0){
-    return (
-      <div>The App is used by pressing the buttons</div>
-    )
-  } 
-  return (
-    <p>Button press History : {allClicks.join(' ')} </p>
-  )
+  return ()=> console.log('Hello', who);
 }
 
-const Button = ({onClick, text}) => <button onClick = {onClick}>{text}</button>
-const App = () =>{
-
-const [left, setLeft]= useState(0);
-const [right, setRight]= useState(0);
-const [allClicks, setAll] = useState([]);
-const [total, setTotal] = useState(0);
-
-const handleLeftClick = ()=>{
-  setAll(allClicks.concat('L'));
-  const updatedLeft = left + 1;
-  setLeft(updatedLeft);
-  setTotal(updatedLeft + right)
-}
-
-const handleRightClick = ()=>{
-  setAll(allClicks.concat('R'));
-  const updatedRight = right +1;
-  setRight(updatedRight);
-  setTotal(updatedRight + left);
-}
  
+  const App = () => {
+  const [value, setValue] = useState(10)
+  
+
+  const setToValue = (newValue) => ()=> {
+    console.log('value now', newValue)  // print the new value to console
+    setValue(newValue)
+  }
+  
   return (
     <div>
-      {left}
-      <Button onClick = {handleLeftClick} text = "Left"/>
-      <Button onClick = {handleRightClick} text = "Right"/>
-      {right} 
-      <History allClicks = {allClicks}/>
-      <p>Total of clicks : {total} </p>
+      {value}
+
+      <button onClick={setToValue(1000)}>thousand</button>
+      <button onClick={setToValue(0)}>reset</button>
+      <button onClick={setToValue(value + 1)}>increment</button>
     </div>
   )
 }
+
+
+
 
 export default App;
