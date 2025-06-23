ef5d71611c6f90ccfce0daf859742d2e358c098c
AGUEH Barnabé Prudent Mahugnon <111515950+Barnab1@users.noreply.github.com>
2025-06-23 17:51:20 +0000

    Did exercice 1.7 of part1

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -8,6 +8,22 @@ const App = ()=>{
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
+  const all = (numbers)=>numbers.reduce((prev, acc)=> prev + acc,0);
+
+  const average = (impressions)=>{
+  if(all(impressions) === 0){
+    return 0;
+  }
+
+   return ((impressions[0] *1)+(impressions[1]*0) + impressions[2]*(-1))/all(impressions)
+  }
+  const positive = (impressions)=>
+    { 
+      if(all(impressions) === 0){
+      return 0;
+      }
+      return impressions[0]/all(impressions)
+    } 
   return (
     <div>
       <h1>give feedback</h1>
@@ -20,6 +36,10 @@ const App = ()=>{
       <div>good {good}</div>
       <div>neutral {neutral} </div>
       <div>bad {bad}</div>
+      <div>all {all([good,neutral,bad])}</div>
+      <div>average {average([good,neutral,bad])} </div>
+      <div>positive {positive([good, neutral, bad])} %</div>
+
     </div>
   )
   }
