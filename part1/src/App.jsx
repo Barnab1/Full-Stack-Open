1f40e765967a6c4995a1c2f013fc3cc291750e06
AGUEH Barnabé Prudent Mahugnon <111515950+Barnab1@users.noreply.github.com>
2025-06-27 11:47:32 +0000

    Did the exercice 1.12"

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,89 +1,32 @@
 import {useState} from 'react';
 
 const Button = ({onClick, text})=> <button onClick={onClick}>{text}</button>
+const App = () => {
 
-const StatisticLine = ({text, value})=>{
-  return (
-    <tr>
-      <td>{text}</td>
-      <td>{value}</td>
-    </tr>
+  const anecdotes = [
+    'If it hurts, do it more often.',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.',
+    'The only way to go fast, is to go well.'
+  ]
+   
+  const [selected, setSelected] = useState(0)
+
+  /**
+   * Select a random number based on array length
+   */
+  const randomSelecter = (array)=> Math.floor(Math.random() * array.length );
   
-)
-}
-const Statistics = (props)=>{
-
-  let good = props.good;
-  let neutral = props.neutral;
-  let bad = props.bad;
-
-  if(good === 0 & neutral === 0 & bad === 0){
-    return ( 
-      <div>No feedback given</div>
-    )
-  }
-
-  const all = (numbers)=>numbers.reduce((prev, acc)=> prev + acc,0);
-
-  const average = (impressions)=>{
-  if(all(impressions) === 0){
-    return 0;
-  }
-
-   return ((impressions[0] *1)+(impressions[1]*0) + impressions[2]*(-1))/all(impressions)
-  }
-  
-  const positive = (impressions)=>
-    { 
-      if(all(impressions) === 0){
-      return 0;
-      }
-      return `${impressions[0]/all(impressions)} %`;
-    }
-
   return (
     <div>
-      <h2>Statistics</h2>
-      <table>
-        <thead>
-        <tr>
-          <th></th>
-          <th></th>
-        </tr>
-        </thead>
-        <tbody>
-      <StatisticLine text="good" value = {good}/>
-      <StatisticLine text="neutral" value = {neutral}/>
-      <StatisticLine text="bad" value = {bad}/>
-      <StatisticLine text="all" value = {all([good,neutral,bad])}/>
-      <StatisticLine text="average" value = {average([good,neutral,bad])}/>
-      <StatisticLine text="positive" value = {positive([good,neutral,bad])}/>
-      </tbody>
-      </table>
+      {anecdotes[selected]}
+      <Button onClick={()=>setSelected(randomSelecter(anecdotes))} text="next anecdote"/>
     </div>
   )
 }
-
-const App = ()=>{
-  // save clicks of each button to its own state
-  const [good, setGood] = useState(0)
-  const [neutral, setNeutral] = useState(0)
-  const [bad, setBad] = useState(0)
-
- 
-  return (
-    <div>
-      <h1>give feedback</h1>
-      <Button onClick ={()=>setGood(good+1)} text="good"/>
-      <Button onClick ={()=>setNeutral(neutral+1)} text="neutral"/>
-      <Button onClick ={()=>setBad(bad +1)} text="bad"/>
-
-      <Statistics good={good} neutral={neutral} bad={bad}/>
-       
-
-    </div>
-  )
-  }
-
 
 export default App
