7ce584650f427cf4caf052d5fbb08ae06ea45310
AGUEH Barnabé Prudent Mahugnon <111515950+Barnab1@users.noreply.github.com>
2025-07-08 16:16:54 +0000

    Did the exercise 2.9 about implementing a search functionality

--- part2/src/App.jsx
+++ part2/src/App.jsx
@@ -1,12 +1,17 @@
 import { useState } from 'react'
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-1234567'},
+   const [persons, setPersons] = useState([
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
+  ])
 
-  ]) 
   const [newName, setNewName] = useState('');
   const [newNumber, setNewNumber] = useState('');
+
+  const [filteredPersons, setFilteredPersons] = useState(persons);
 
   const handleNewName = (event)=>{
     console.log(event.target.value);
@@ -19,6 +24,7 @@ const App = () => {
   }
 
   const add = (event)=>{
+
   event.preventDefault();
   const findNameAdded = persons.find((person)=>person.name === newName);
 
@@ -28,21 +34,50 @@ const App = () => {
   }else{
   const newNameObj = {
     name: newName,
-    number: newNumber
+    number: newNumber,
+    id: persons.length +1
   }
-  setPersons(persons.concat(newNameObj));
+
+  const updated = persons.concat(newNameObj) ;
+  setPersons(updated);
+  setFilteredPersons(updated);
+
   setNewName('');
   setNewNumber('');
   }
   }
 
+
+
+  const filterResult = (event)=>{
+
+    const search = event.target.value.toLowerCase();
+
+    if(search == ""){
+      setFilteredPersons(persons);
+    }else{
+      const filtered = persons.filter((person)=>
+        person.name.toLowerCase().includes(search)
+      );
+
+      setFilteredPersons(filtered);
+    }
+  } 
+  
   return (
     <div>
-      <h2>Phonebook</h2>
+      <h1>Phonebook</h1>
 
+      <div>
+        Filter shown with 
+        <input type="text"  onChange={filterResult}/>
+      </div>
+
+    <h2>Add a new </h2>
       <form>
 
         <div>
+
           Name: <input type="text" value={newName} onChange={handleNewName}/>
         </div>
 
@@ -57,14 +92,18 @@ const App = () => {
       </form>
 
       <h2>Numbers</h2>
+
+      <div>
         {
-        persons.map(person =>
+        filteredPersons.map(person =>
           <p key={person.name}>{person.name} {person.number}</p>
         )
         }
+        </div>
     </div>
   )
-}
 
+
+  }
 
 export default App ;
