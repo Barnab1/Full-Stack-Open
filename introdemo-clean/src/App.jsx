de689685caeae60115efeae7bc629f530f281c2a
AGUEH Barnabé Prudent Mahugnon <111515950+Barnab1@users.noreply.github.com>
2025-09-22 07:27:24 +0000

    I ended the second section course

--- introdemo-clean/src/App.jsx
+++ introdemo-clean/src/App.jsx
@@ -1,103 +1,51 @@
 import {useState, useEffect} from 'react';
-import Note from './components/Note';
-import Notification from './components/Notification';
-import noteService from './services/notes';
-import Footer from './components/Footer';
+import axios from 'axios';
+
 
 const App = ()=>{
 
-  const [notes, setNotes] = useState([]);
-  const [newNote, setNewNote] = useState('A new Note...');
-  const [showAll, setShowAll] = useState(true);
-  const [errorMessage, setErrorMessage] = useState("some error happened ...");
+  const [value, setValue] = useState('');
+  const  [rates, setRates] = useState({});
+  const [currency, setCurrency] = useState({});
 
-  const hook = ()=>{
-     console.log('effect');
-    noteService
-    .getAll()
-    .then((initialNotes)=>{
-      console.log('Promise fulfilled');
-      setNotes(initialNotes);
-    })
-  }
+  useEffect(()=>{
 
-  useEffect(hook,[])
+    console.log('Effect run, currency is now: ');
+    //Skip if currency is not defined
 
-  console.log('render', notes.length, 'notes');
+    if(currency){
+      console.log("Fetching exchange rate...");
 
-  const notesToShow = showAll ? notes : notes.filter((note)=> note.important);
-
-
-  const addNote= (event)=>{
-    event.preventDefault();
-
-    const noteObject = {
-      content: newNote,
-      important: Math.random() < 0.5
+      axios
+      .get(`https://open.er-api.com/v6/latest/${currency}`)
+      .then(response=>{
+        setRates(response.data.rates);
+      })
     }
 
-   noteService
-    .create(noteObject)
-    .then(returnedNote =>{
-      setNotes(notes.concat(returnedNote));
-      setNewNote(''); // clear input content after saving its previous value
-    })
-    
+  },[currency])
+
+
+  const handleChange = (event)=>{
+    setValue(event.target.value);
   }
 
-  
-  const handleNoteChange = (event)=>{
-    console.log(event.target.value);
-    setNewNote(event.target.value);
+  const onSearch = (event)=>{
+    event.preventDefault();
+    setCurrency(value);
   }
 
-  const toggleImportanceOf= (id)=>{
-    console.log(`Importance of ${id} needs to be toggled`);
-    const note = notes.find(n=> n.id === id);
-    const changedNote = {...note, important: !note.important};
-
-   noteService
-   .update(id,changedNote)
-   .then(returnedNote =>{
-    setNotes(notes.map(note=> note.id === id ? returnedNote: note));
-   })
-   .catch(error=>{
-        setErrorMessage(`Note:  '${note.content}' was already deleted from the server`);
-        setTimeout(()=>{
-          setErrorMessage(null);
-        },5000);
-
-        setNotes(notes.filter(n=> n.id !== id));
-   })
-  }
-
-  return(
+  return (
     <div>
-      <h1>Notes</h1>
-      <Notification message={errorMessage}/>
-      <div>
-        <button onClick={()=> setShowAll(!showAll)}>
-          show {showAll ? 'Important': 'All'}
-        </button>
-      </div>
-      <ul>
-        {
-        notesToShow.map((note)=>
-          <Note 
-        key= {note.id} 
-        note={note }
-        toggleImportance = {()=>toggleImportanceOf(note.id)} 
-        />)
-        }
-      </ul>
-      <form onSubmit={addNote}>
-        <input value={newNote}
-          onChange={handleNoteChange}
-        />
-        <button type="submit">Save</button>
-      </form>
+    <form onSubmit = {onSearch}>
+      currency: <input value={value} onChange ={handleChange}/>
+      <button type="submit">exchange rate</button>
+    </form>
 
-      <Footer />
+    <pre>
+      {JSON.stringify(rates,null,2)}
+
+    </pre>
     </div>
   )
 }
