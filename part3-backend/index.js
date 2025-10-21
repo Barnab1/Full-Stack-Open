57748360ec9d5a7e7a4de4d8ec41a4ee079e6920
AGUEH Barnabé Prudent Mahugnon <111515950+Barnab1@users.noreply.github.com>
2025-10-21 13:23:55 +0000

    update README.md

--- part3-backend/index.js
+++ part3-backend/index.js
@@ -1,60 +1,34 @@
 const express = require('express');
 const app = express();
 
+app.use(express.json())
 
 let notes = [
-    {
-    id: "1",
-    content: "HTML is easy",
-    important: true
-  },
   {
-    id: "2",
-    content: "Browser can execute only JavaScript",
-    important: false
+    id: 1,
+    note: "I love Paris",
+    important: "True"
   },
+   {
+    id: 2,
+    note: "He loves Paris",
+    important: "True"
+  },
+
   {
-    id: "3",
-    content: "GET and POST are the most important methods of HTTP protocol",
-    important: true
+    id: 3,
+    note: "We loves Paris",
+    important: "True"
   }
 ]
-
-
-app.get('/',(request, response)=>{
-  response.send('<h1>Hello World</h1>');
-})
-
-
-app.get('/api/notes',(resquest, response)=>{
+app.get('/api/notes',(request, response)=>{
   response.json(notes);
+  console.log(notes);
 })
-
-app.get('/api/notes/:id',(request, response)=>{
-  const id = request.params.id
-  const note = notes.find(note => note.id === id);
-  
-  if(note){
-    response.json(note);
-  }else{
-    response.status(400).end()
-  }
-
-})
-
-
-//Deleting resources
-/*
-app.delete('/api/notes/:id',(request, response)=>{
-  const id = request.params.id;
-  notes = notes.filter(note => note.id !== id);
-
-  response.status(204).end();
-})
-*/
 
 
 const PORT = 3001;
 app.listen(PORT,()=>{
-  console.log(`Server running on port ${PORT}`)
+  console.log('Even more excited');
 })
+  
