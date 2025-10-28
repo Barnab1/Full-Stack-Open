9062b2d9616d1f8cd6254e68dfe19e7c892c48fe
AGUEH Barnabé Prudent Mahugnon <111515950+Barnab1@users.noreply.github.com>
2025-10-28 11:06:34 +0000

    Did the course part before first exercises

--- part3-backend/index.js
+++ part3-backend/index.js
@@ -21,9 +21,43 @@ let notes = [
     important: "True"
   }
 ]
+
+app.get('/',(request,response)=>{
+  response.send('<h1>Hello World</h1>');
+})
 app.get('/api/notes',(request, response)=>{
   response.json(notes);
   console.log(notes);
+})
+
+const generatedId = ()=>{
+  const maxId = notes.length > 0 ? 
+    Math.max(...notes.map(n => Number(n.id))) : 0;
+  
+  return String(maxId +1);
+}
+
+app.post('/api/notes',(request, response)=>{
+
+  //fetching the request body
+  const body = request.body;
+
+  //checking the content existence
+  if(!body.content){
+    return response.status(400).json({
+      error: 'Content missing'
+    });
+  }
+  
+  const note = {
+    content: body.content,
+    important: body.important || false,
+    id: generatedId()
+  }
+
+
+  notes = notes.concat(note);
+  response.json(note)
 })
 
 
