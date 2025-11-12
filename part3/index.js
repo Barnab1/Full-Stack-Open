31ae53abd7209bc764b422deef9a6f1a1f3807a1
AGUEH Barnabé Prudent Mahugnon <111515950+Barnab1@users.noreply.github.com>
2025-11-12 20:30:52 +0000

    Did the exercise 3.6 about preventing blank and duplicate values

--- part3/index.js
+++ part3/index.js
@@ -70,26 +70,41 @@ const generatedId = ()=>{
 app.post('/api/persons',(request, response)=>{
   const body= request.body;
 
-  console.log('Here is the requested body', body);
-  //response.json(person);
+  //console.log('Here is the requested body', body);
 
-  
   if(!body){
     return response.status(400).json({'error':"content missing"});
   }
 
-
-  const person = {
+// creating a new entry
+  const newPerson = {
     'id': generatedId(),
     'name':body.name,
     'number':body.number
   }
 
-  persons = persons.concat(person);
+//Checking name or number 
+  if(newPerson.name == "" || newPerson.number == "" ){
+    return response.status(400).json({'error':"Please fill in both name and number"});
+  }
 
+  //ending the session  if the name already exist
+  const isNameExisting = persons.find(people => people.name == newPerson.name);
+
+  //console.log(isNameExisting);
+  if(isNameExisting != undefined){
+
+    return response.status(400).json({'error':"name must be unique"})
+  }
+
+//concatenating the anwser and returning it
+  persons = persons.concat(newPerson);
   response.json(persons);
-
+console.log('New entry was succesfully added');
 })
+
+
+
 const PORT = 3001;
 app.listen(PORT, ()=>{
     console.log('Application working');
